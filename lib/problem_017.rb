4c530ee6970f1d16b09162cccfd5dcf98f4f2a2d
Harry Steinhilber, Jr <harry.steinhilber@kohls.com>
2011-02-07 16:23:57 -0500

    problem 17 complete

--- lib/problem_017.rb
+++ lib/problem_017.rb
@@ -1,28 +1,65 @@
 class Problem017
 
   class ::Integer
-    TEENS = ['','one','two','three','four','five',
-      'six','seven','eight','nine','ten','eleven','twelve',
-      'thirteen','fourteen','fifteen','sixteen','seventeen',
-      'eighteen','ninteen']
-    LARGER = ['','','twenty','thirty','forty','fifty','sixty','seventy',
-      'eighty','ninety']
+    WORDS = {
+      1 => 'one',
+      2 => 'two',
+      3 => 'three',
+      4 => 'four',
+      5 => 'five',
+      6 => 'six',
+      7 => 'seven',
+      8 => 'eight',
+      9 => 'nine',
+      10 => 'ten',
+      11 => 'eleven',
+      12 => 'twelve',
+      13 => 'thirteen',
+      14 => 'fourteen',
+      15 => 'fifteen',
+      16 => 'sixteen',
+      17 => 'seventeen',
+      18 => 'eighteen',
+      19 => 'nineteen',
+      20 => 'twenty',
+      30 => 'thirty',
+      40 => 'forty',
+      50 => 'fifty',
+      60 => 'sixty',
+      70 => 'seventy',
+      80 => 'eighty',
+      90 => 'ninety'
+    }
+
     def to_words
+      thousands = self / 1000
+      hundreds = (self % 1000) / 100
+      tens = (self % 100 / 10) * 10 
+      ones = (self % 10) + (tens == 10 ? 10 : 0)
+      tens = 0 if tens == 10
+
       result = ''
-
-      tens = self / 10
-      teens = self % 10 + (tens == 1 ? 10 : 0)
-      tens = 0 if tens == 1
-
-      result = LARGER[tens]
-      result += '-' if tens > 1 and teens > 0
-      result += TEENS[teens]
+      result << WORDS[thousands] + ' thousand' if thousands > 0
+      result << WORDS[hundreds] + ' hundred' if hundreds > 0
+      result << ' and ' if hundreds > 0 and (tens > 0 or ones > 0)
+      result << WORDS[tens] if tens > 0
+      result << '-' if tens > 0 and ones > 0
+      result << WORDS[ones] if ones > 0
 
       return result
     end
   end
 
+  def count_letters(words)
+    words.split('').count { |c| c =~ /[a-zA-Z]/ }
+  end
+
+  def sum_to(max) 
+    1.upto(max).inject(0) { |sum,n| sum + count_letters(n.to_words) }
+  end
+
   def run
+    sum_to(1_000)
   end
 end
 
--- spec/problem_017_spec.rb
+++ spec/problem_017_spec.rb
@@ -4,7 +4,7 @@ require 'problem_017'
 describe Problem017 do
 
   before(:each) do
-    @prb = Problem017
+    @prb = Problem017.new
   end
 
   describe "converting numbers to words" do
@@ -81,8 +81,8 @@ describe Problem017 do
       18.to_words.should == 'eighteen'
     end
 
-    it "converts 19 to 'ninteen'" do
-      19.to_words.should == 'ninteen'
+    it "converts 19 to 'nineteen'" do
+      19.to_words.should == 'nineteen'
     end
     
     it "converts 20 to 'twenty'" do
@@ -125,33 +125,60 @@ describe Problem017 do
       90.to_words.should == 'ninety'
     end
 
-    it "converts 100 to 'one hundred'"
+    it "converts 100 to 'one hundred'" do
+      100.to_words.should == 'one hundred'
+    end
 
-    it "converts 300 to 'three hundred'"
+    it "converts 300 to 'three hundred'" do
+      300.to_words.should == 'three hundred'
+    end
 
-    it "converts 103 to 'one hundred and three'"
+    it "converts 103 to 'one hundred and three'" do
+      103.to_words.should == 'one hundred and three'
+    end
 
-    it "converts 1000 to 'one thousand'"
+    it "converts 1000 to 'one thousand'" do
+      1000.to_words.should == 'one thousand' 
+    end
 
-    it "converts 342 to 'three hundred and forty-two'"
+    it "converts 342 to 'three hundred and forty-two'" do
+      342.to_words.should == 'three hundred and forty-two'
+    end
 
-    it "converts 115 to 'one hundred and fifteen'"
+    it "converts 115 to 'one hundred and fifteen'" do
+      115.to_words.should == 'one hundred and fifteen'
+    end
   end
 
   describe "computing total letters" do
 
-    it "counts the letters in 'one' as 3"
+    it "counts the letters in 'one' as 3" do
+      @prb.count_letters('one').should == 3
+    end
 
-    it "counts the letters in 'twenty-one' as 9"
+    it "counts the letters in 'twenty-one' as 9" do
+      @prb.count_letters('twenty-one').should == 9
+    end
 
-    it "counts the letters in 'one hundred and twenty-one' as 22"
+    it "counts the letters in 'one hundred and twenty-one' as 22" do
+      @prb.count_letters('one hundred and twenty-one').should == 22
+    end
 
-    it "computes 3 letters for numbers up to 1 ('one')"
+    it "computes 3 letters for numbers up to 1 ('one')" do
+      @prb.sum_to(1).should == 3
+    end
     
-    it "computes 11 letters for numbers up to 3 ('one'+'two'+'three')"
+    it "computes 11 letters for numbers up to 3 ('one'+'two'+'three')" do
+      @prb.sum_to(3).should == 11
+    end
 
-    it "computes 19 letters for numbers up to 5 ('one'+'two'+'three'+'four'+'five')"
+    it "computes 19 letters for numbers up to 5 ('one'+'two'+'three'+'four'+'five')" do
+      @prb.sum_to(5).should == 19
+    end
   end
 
-  it "computes the total letters for all numbers up to 1,000"
+  it "computes the total letters for all numbers up to 1,000" do
+    @prb.should_receive(:sum_to).with(1_000).and_return(999)
+    @prb.run.should == 999
+  end
 end
