ece346f38e781d0155af59f2117e7b0998d86185
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2011-02-21 12:27:45 -0500

    simplified handling of composites hash

--- lib/prime_generator.rb
+++ lib/prime_generator.rb
@@ -5,22 +5,16 @@ class PrimeGenerator
 
   def each
     composites = {}
-    def composites.combine!(hash) 
-      merge!(hash) do |key,x,y|
-        x.push(y.shift) while y.any?; x
-      end
-    end
-
     n = 2
     while true
-      unless composites.has_key?(n)
-        composites.combine!(n**2 => [n])
-        yield n
-      else
+      if composites.has_key?(n)
         factors = composites[n]
-        factors.each { |factor| composites.combine!(n + factor => [factor]) }
+        factors.each { |factor| composites.merge!(n + factor => [factor]) { |key,left,right| left + right } }
         composites.delete(n)
-      end
+      else
+        composites.merge!(n**2 => [n]) { |key,left,right| left + right }
+        yield n
+     end
       n += 1
     end
   end
