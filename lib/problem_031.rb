bcc702a9353bc5a73f2c3b8037ef33c53577eb82
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2011-02-21 00:23:47 -0500

    modified problem 31 for better testability and easier to understand result

--- lib/problem_031.rb
+++ lib/problem_031.rb
@@ -1,34 +1,26 @@
 class Problem031
 
-  def compute_value(two_pound=0, one_pound=0, fifty_pence=0, twenty_pence=0, ten_pence=0, five_pence=0, two_pence=0, one_pence=0)
-    two_pound*200 + one_pound*100 + fifty_pence*50 + twenty_pence*20 + ten_pence*10 + five_pence*5 + two_pence*2 + one_pence
+  def next_coin(value)
+    return 100 if value == 200
+    return 50 if value == 100
+    return 20 if value == 50
+    return 10 if value == 20
+    return 5 if value == 10
+    return 2 if value == 5
+    1
+  end
+  
+  def count_coin_combinations(total, value) 
+    return 1 if total == 0 or value == 1
+    result = 0
+    total.step(0,-value) do |new_total|
+      result += count_coin_combinations(new_total, next_coin(value))
+    end
+    return result
   end
 
   def run
-    count = 0
-    max_two_pound = 200 / 200
-    (0..max_two_pound).each do |two_pound|
-      max_one_pound = (200 - compute_value(two_pound)) / 100
-      (0..max_one_pound).each do |one_pound|
-        max_fifty_pence = (200 - compute_value(two_pound,one_pound)) / 50
-        (0..max_fifty_pence).each do |fifty_pence|
-          max_twenty_pence = (200 - compute_value(two_pound,one_pound,fifty_pence)) / 20
-          (0..max_twenty_pence).each do |twenty_pence|
-            max_ten_pence = (200 - compute_value(two_pound,one_pound,fifty_pence,twenty_pence)) / 10
-            (0..max_ten_pence).each do |ten_pence|
-              max_five_pence = (200 - compute_value(two_pound,one_pound,fifty_pence,twenty_pence,ten_pence)) / 5
-              (0..max_five_pence).each do |five_pence|
-                max_two_pence = (200 - compute_value(two_pound,one_pound,fifty_pence,twenty_pence,ten_pence,five_pence)) / 2
-                (0..max_two_pence).each do |two_pence|
-                  count += 1 # Number of one pence pieces must be the remainder, so only one possiblity
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-    return count
+    count_coin_combinations(200,200)
   end
 end
 
--- spec/problem_031_spec.rb
+++ spec/problem_031_spec.rb
@@ -4,45 +4,66 @@ require 'problem_031'
 describe Problem031 do
   let(:prb) { Problem031.new }
 
-  describe "computing total coin value" do
+  describe "iterating coins" do
 
-    it "gives a 1p piece a value of 1" do
-      prb.compute_value(0,0,0,0,0,0,0,1).should == 1
+    it "returns 1L after 2L" do
+      prb.next_coin(200).should == 100
     end
 
-    it "gives a 2p piece a value of 2" do 
-      prb.compute_value(0,0,0,0,0,0,1,0).should == 2
+    it "returns 50p after 1L" do
+      prb.next_coin(100).should == 50
     end
 
-    it "gives a 5p piece a value of 5" do
-      prb.compute_value(0,0,0,0,0,1,0,0).should == 5
+    it "returns 20p after 50p" do
+      prb.next_coin(50).should == 20
     end
 
-    it "gives a 10p piece a value of 10" do
-      prb.compute_value(0,0,0,0,1,0,0,0).should == 10
+    it "returns 10p after 20p" do
+      prb.next_coin(20).should == 10
     end
 
-    it "gives a 20p piece a value of 20" do
-      prb.compute_value(0,0,0,1,0,0,0,0).should == 20
+    it "returns 5p after 10p" do
+      prb.next_coin(10).should == 5
     end
 
-    it "gives a 50p piece a value of 50" do
-      prb.compute_value(0,0,1,0,0,0,0,0).should == 50
+    it "returns 2p after 5p" do
+      prb.next_coin(5).should == 2
     end
 
-    it "gives a 1L piece a value of 100" do
-      prb.compute_value(0,1,0,0,0,0,0,0).should == 100
-    end
-
-    it "gives a 2L piece a value of 200" do
-      prb.compute_value(1,0,0,0,0,0,0,0).should == 200
-    end
-
-    it "gives 1L + 50p + 2x20p + 5p + 2p + 3x1p pieces a value of 200" do
-      prb.compute_value(0,1,1,2,0,1,1,3).should == 200
+    it "returns 1p after 2p" do
+      prb.next_coin(2).should == 1
     end
   end
 
-  # TODO: need to test that it iterates only combinations <= 200 total value
-  # TODO: need to test that it counts the number of combinations = 200 total value
+  describe "counting coin combinations" do
+
+    it "returns 1 way to get 0L using 1p pieces" do
+      prb.count_coin_combinations(0,1).should == 1
+    end
+
+    it "returns 1 way to get 0L using <=2L pieces" do
+      prb.count_coin_combinations(0,200).should == 1
+    end
+
+    it "returns 1 way to get 200L using 1p pieces" do
+      prb.count_coin_combinations(200,1).should == 1
+    end
+
+    it "returns 1 way to get 100L using 1p pieces" do
+      prb.count_coin_combinations(100,1).should == 1
+    end
+
+    it "returns 3 ways to get 5p using <=2p pieces" do
+      prb.count_coin_combinations(5,2).should == 3
+    end
+
+    it "returns 4 ways to get 5p using <=5p pieces" do
+      prb.count_coin_combinations(5,5).should == 4
+    end
+  end
+
+  it "computes the number of combinations to get 2L using <=2L pieces" do
+    prb.should_receive(:count_coin_combinations).with(200,200).and_return(50)
+    prb.run.should == 50
+  end
 end
