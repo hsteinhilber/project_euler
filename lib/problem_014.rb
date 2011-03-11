66bfbf279c8f264c3f62a8c6f7b167433e4723ef
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2011-03-10 22:37:56 -0500

    sequence now only returns the next value, all lengths are added to the known list, not just the currently requested one

--- lib/problem_014.rb
+++ lib/problem_014.rb
@@ -4,22 +4,14 @@ class Problem014
     @known_lengths = { 1 => 1 }
   end
 
-  def sequence(start)
-    Enumerator.new do |yielder|
-      n = start
-      (yielder << n) && (n = n.even? ? n / 2 : 3*n + 1) while n > 1
-      yielder << 1
-    end
+  def sequence(n)
+    return nil if n <= 1
+    n.even? ? n / 2 : 3*n + 1
   end
 
   def chain_length(start)
     return @known_lengths[start] if @known_lengths.include? start
-
-    count = 0
-    sequence(start).each do |term|
-      return @known_lengths[start] = @known_lengths[term] + count if @known_lengths.include? term
-      count += 1
-    end
+    @known_lengths[start] = chain_length(sequence(start)) + 1
   end
 
   def chain_lengths
--- spec/problem_014_spec.rb
+++ spec/problem_014_spec.rb
@@ -9,16 +9,52 @@ describe Problem014 do
 
   describe "generating the sequence" do
 
-    it "has the terms 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 when starting with 13" do
-      @prb.sequence(13).to_a.should == [13,40,20,10,5,16,8,4,2,1]
+    it "returns 40 when given 13" do
+      @prb.sequence(13).should == 40 
     end
 
-    it "has the terms 5, 16, 8, 4, 2, 1 when starting with 5" do
-      @prb.sequence(5).to_a.should == [5,16,8,4,2,1]
+    it "returns 20 when given 40" do
+      @prb.sequence(40).should == 20
     end
 
-    it "has the terms 2, 1 when starting with 2" do
-      @prb.sequence(2).to_a.should == [2, 1]
+    it "returns 10 when given 20" do
+      @prb.sequence(20).should == 10 
+    end
+
+    it "returns 5 when given 10" do
+      @prb.sequence(10).should == 5 
+    end
+
+    it "returns 16 when given 5" do
+      @prb.sequence(5).should == 16 
+    end
+
+    it "returns 8 when given 16" do
+      @prb.sequence(16).should == 8 
+    end
+
+    it "returns 4 when given 8" do
+      @prb.sequence(8).should == 4 
+    end
+
+    it "returns 2 when given 4" do
+      @prb.sequence(4).should == 2 
+    end
+
+    it "returns 1 when given 2" do
+      @prb.sequence(2).should == 1 
+    end
+
+    it "returns nil when given 1" do
+      @prb.sequence(1).should be_nil 
+    end
+
+    it "returns 904 when given 1808" do
+      @prb.sequence(1808).should == 904 
+    end
+
+    it "returns 904 when given 301" do
+      @prb.sequence(301).should == 904 
     end
   end
 
