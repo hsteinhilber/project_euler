1065ecc533816940710ae2a5ba34a1cce33a9c6a
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2011-03-25 23:28:41 -0400

    constants used to remove 'magic' numbers

--- lib/problem_017.rb
+++ lib/problem_017.rb
@@ -5,12 +5,16 @@ class Problem017
     11 => 6, 12 => 6, 13 => 8, 14 => 8, 15 => 7, 16 => 7, 17 => 9, 18 => 8, 19 => 8,
     20 => 6, 30 => 6, 40 => 5, 50 => 5, 60 => 5, 70 => 7, 80 => 6, 90 => 6, 0 => 0
   }
+
+  ONE_THOUSAND = 11
+  HUNDRED = 7
+  AND = 3
  
   def letters_for(n)
     sum = 0
-    sum += 11 and n %= 1000 if n >= 1000
-    sum += LETTERS[n / 100] + 7 and n %= 100 if n >= 100
-    sum += 3 if sum > 0 and n > 0
+    sum += ONE_THOUSAND and n %= 1000 if n >= 1000
+    sum += LETTERS[n / 100] + HUNDRED and n %= 100 if n >= 100
+    sum += AND if sum > 0 and n > 0
     sum += LETTERS[n - n%10] + LETTERS[n%10] and n = 0 if n >= 20
     sum += LETTERS[n]
   end
