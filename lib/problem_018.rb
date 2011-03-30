8e70e8e5e8678beaa2c7f5bb638d87a7d4e88a1c
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2011-03-30 18:03:07 -0400

    added caching so subtriangles are not re-evaluated

--- lib/problem_018.rb
+++ lib/problem_018.rb
@@ -18,13 +18,21 @@ class Problem018
                [  4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23 ]
              ]
 
+  def initialize()
+    @cache = {}
+  end
+
   def add_triangle(triangle, row = 0, col = 0)
     raise "Invalid row or column" if col > row or col < 0
     return 0 if row >= triangle.length
     return triangle[row][col] if row == triangle.length - 1
+    return @cache[row][col] if @cache.include? row and @cache[row].include? col
+
     left = add_triangle(triangle, row + 1, col)
     right = add_triangle(triangle, row + 1, col + 1)
-    triangle[row][col] + (left > right ? left : right)
+
+    @cache[row] ||= {}
+    @cache[row][col] ||= triangle[row][col] + (left > right ? left : right)
   end
 
   def run
