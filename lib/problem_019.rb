43acd84048bf0599f7580792a6624fa4ff72eeca
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2011-05-08 22:37:51 -0400

    simplified implementation of count_sunday on prb 19

--- lib/problem_019.rb
+++ lib/problem_019.rb
@@ -2,11 +2,7 @@ class Problem019
 
   def count_sundays(year_range)
     year_range = year_range..year_range if year_range.kind_of?(Integer)
-    year_range.inject(0) do |count,year|
-      count + (1..12).inject(0) do |count,month|
-        count + (Time.new(year, month, 1).sunday? ? 1 : 0)
-      end
-    end
+    (1..12).to_a.product(year_range.to_a).count { |month, year| Time.new(year, month, 1).sunday? }
   end
 
   def run
