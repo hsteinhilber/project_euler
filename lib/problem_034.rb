1270e1d7e09e8efc6cbfcc889af82d0bf4bc2388
Harry Steinhilber <harry.steinhilber@live.com>
2015-01-22 06:15:52 -0500

    incomplete update

--- lib/problem_034.rb
+++ lib/problem_034.rb
@@ -2,17 +2,21 @@ require 'integer'
 
 class Problem034
 
+  MAX_VAL = 7 * (9*8*7*6*5*4*3*2*1)
+  
   # precomputed factorials for 0..9
   @@factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
-  def digit_fact(n)
-    n.digits.map { |d| @@factorials[d] }.reduce(:+)
+  def digit_fact(digits)
+    digits.map { |d| @@factorials[d] }.reduce(:+)
   end
 
   def sequence
     Enumerator.new do |yielder|
-      (10..2540160).each do |n|
-        yielder << n if n == digit_fact(n)
+      (10..MAX_VAL).each do |n|
+        digits = n.digits
+        next if n.odd? and digits.count { |d| d <= 1 }.even?
+        yielder << n if n == digit_fact(digits)
       end
     end
   end
--- spec/problem_034_spec.rb
+++ spec/problem_034_spec.rb
@@ -7,23 +7,23 @@ describe Problem034 do
   describe "computing sum of digit!" do
 
     it "returns 1 for 1" do
-      prb.digit_fact(1).should == 1
+      prb.digit_fact([1]).should == 1
     end
 
     it "returns 2 for 2" do
-      prb.digit_fact(2).should == 2
+      prb.digit_fact([2]).should == 2
     end
 
     it "returns 8 for 301" do
-      prb.digit_fact(301).should == 8
+      prb.digit_fact([3,0,1]).should == 8
     end
 
     it "returns 145 for 145" do
-      prb.digit_fact(145).should == 145
+      prb.digit_fact([1,4,5]).should == 145
     end
 
     it "returns 19 for 3313" do
-      prb.digit_fact(3313).should == 19
+      prb.digit_fact([3,3,1,3]).should == 19
     end
   end
 
@@ -33,15 +33,9 @@ describe Problem034 do
       prb.sequence.should include(145)
     end
 
-    # it "completes in less than 10s" do
-    #   start_time = Time.now
-    #   prb.sequence.to_a
-    #   (Time.now - start_time).should be < 10.0
-    # end
-
     it "contains only numbers whose value equals the sum of digit!" do
       prb.sequence.each do |n|
-        n.should == prb.digit_fact(n)
+        n.should == prb.digit_fact(n.digits)
       end
     end
   end
